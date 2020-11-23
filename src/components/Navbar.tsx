fcdcdbec99a52797900f9de7f87824644d18f6f4
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2020-11-23 02:36:58 +0100

    Remove unnecessary divs from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,28 +53,27 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
   }
 
   return (
-    <Box p={4}>
-      <Wrapper
-        size="large"
-        padding="none"
-        borderBottom="1px solid black"
-      >
-        <Flex align="center" justify="space-between">
-          <NextLink href="/">
-            <Link>
-              <Image
-                mr={4}
-                rounded="full"
-                size="40px"
-                src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Flogok.org%2Fwp-content%2Fuploads%2F2015%2F03%2FHublot-logo-350x302.png&f=1&nofb=1"
-                alt="Himpun Logo"
-              />
-            </Link>
-          </NextLink>
+    <Wrapper
+      size="large"
+      padding="regular"
+      borderBottom="1px solid black"
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <NextLink href="/">
+        <Link>
+          <Image
+            mr={4}
+            rounded="full"
+            size="40px"
+            src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Flogok.org%2Fwp-content%2Fuploads%2F2015%2F03%2FHublot-logo-350x302.png&f=1&nofb=1"
+            alt="Himpun Logo"
+          />
+        </Link>
+      </NextLink>
 
-          {body}
-        </Flex>
-      </Wrapper>
-    </Box>
+      {body}
+    </Wrapper>
   );
 }
